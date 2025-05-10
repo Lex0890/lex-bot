e5eb7fbf7a694f6f59dcc1a7e96cd9b290f12cf8
Lex0890 <turbi0111@gmail.com>
2025-05-10 00:25:37 -0400

    Integration with mongodb has been added and a bug in the ready event has been fixed.

--- src/events/ready/ready.js
+++ src/events/ready/ready.js
@@ -3,9 +3,10 @@ export default (client) => {
     try{
         const guilds = client.guilds.cache;
         console.log(`-- Loaded in ${guilds.size} guilds`)
-        for (const guild of guilds){
+        guilds.forEach(guild =>{
             console.log(`-- ${guild.name} (${guild.id})`)
-        }
+        })
+        console.log(`bot started as: ${client.user.username}`)
     }catch(error){
         console.error(`-- there was an error: ${error.message}`)
     }
--- src/index.js
+++ src/index.js
@@ -6,6 +6,8 @@ import { CommandKit } from 'commandkit';
 import { dirname as dn } from 'node:path';
 import { fileURLToPath } from 'node:url';
 
+import mongoose from "mongoose";
+
 const dirname = dn(fileURLToPath(import.meta.url));
 
 const client = new Client({
@@ -22,5 +24,16 @@ new CommandKit({
     eventsPath: `${dirname}/events`,
     commandsPath: `${dirname}/commands`
 });
-
-client.login(process.env.TOKEN);
+(async () => {
+    try {
+        await mongoose.connect(process.env.MONGO_URI)
+        if(mongoose.connection.readyState === 0){
+            throw new Error("there was an error connecting to the data base please review the uri");
+        } else{
+            console.log('-- DB loaded')
+        }
+        await client.login(process.env.TOKEN);
+    } catch (error) {
+        console.error(`there was an error setting up the bot: ${error}`);
+    }
+})();
