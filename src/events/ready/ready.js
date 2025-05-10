7fe27ff41717ec5c06639d66e512488f9aa9d464
Lex0890 <turbi0111@gmail.com>
2025-05-09 23:55:11 -0400

    Implement logging for loaded guilds and error handling on client ready event

--- src/events/ready/ready.js
+++ src/events/ready/ready.js
@@ -1,4 +1,12 @@
 /** * @param {import('discord.js').Client} client */
 export default (client) => {
-    console.log(`${client.user.tag} is online!`);
+    try{
+        const guilds = client.guilds.cache;
+        console.log(`-- Loaded in ${guilds.size} guilds`)
+        for (const guild of guilds){
+            console.log(`-- ${guild.name} (${guild.id})`)
+        }
+    }catch(error){
+        console.error(`-- there was an error: ${error.message}`)
+    }
 }
