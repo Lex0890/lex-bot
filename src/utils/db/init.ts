13ce5f819082ad070c463fc0678e1f9e27eceebd
Sfreilyn Alexander Turbi Disla <turbi0111@gmail.com>
2025-10-02 03:46:25 -0600

    changed psql to url

--- src/utils/db/init.ts
+++ src/utils/db/init.ts
@@ -6,15 +6,7 @@ const __dirname = path.dirname(new URL(import.meta.url).pathname);
 
 const query = fs.readFileSync(path.join(__dirname, 'src', 'utils', 'db', 'init.sql'), 'utf8');
 
-const config: pg.Options<{}> = {
-  user: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
-  host: process.env.DB_HOST,
-  port: 5432,
-  database: process.env.DB_NAME,
-};
-
-const sql = pg(config);
+const sql = pg(process.env.DATABASE_URL as string);
 
 export async function initdb(): Promise<void> {
   await sql.unsafe(query);
