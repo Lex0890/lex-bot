4649571d62e287b7e2de103c15dffd836ec1886c
Sfreilyn Alexander Turbi Disla <turbi0111@gmail.com>
2025-10-02 03:58:22 -0600

    added per guild prefix and caching

--- src/app.ts
+++ src/app.ts
@@ -1,9 +1,21 @@
 import { Client } from 'discord.js';
-import { initdb } from '@/utils/db/init.ts';
+import sql, { initdb } from '@/utils/db/init.ts';
+import { commandkit } from 'commandkit';
 
 const client = new Client({
   intents: ['Guilds', 'GuildMembers', 'GuildMessages', 'MessageContent'],
 });
+
+commandkit.setPrefixResolver(async (message) => {
+  if (!message.guildId) return '!';
+
+  const guild = await sql`SELECT * FROM guilds WHERE id = ${message.guildId}`;
+
+  const prefix = guild[0]?.prefix || '!';
+
+  return prefix;
+});
+
 await initdb();
 
 export default client;
